05b3b76e0e8f215f70505484d41cf0a8b79fd794
Onur Can Bingöl <117171988+Onur-Bingoel@users.noreply.github.com>
2026-03-14 16:15:45 +0100

    fix: remove warnings and add comments to tests

--- FinalProject/Secure_Remote_Access_Protocol/src/tests/test.rs
+++ FinalProject/Secure_Remote_Access_Protocol/src/tests/test.rs
@@ -1,5 +1,5 @@
+#![allow(unused)]
 mod tests {
-    use crate::client::pqtls::pq_tls;
     use crate::crypto::participant::{DatabaseContent, Message, User, CA};
     use crate::{client, crypto, server};
     use elliptic_curve::{Field, Group};
@@ -14,10 +14,13 @@ mod tests {
 
     #[test]
     fn test_register_and_login() {
+        println!("Testing register and login");
+
         let mut ca = CA::new();
         let mut ca_clone = ca.clone();
         let mut g: ProjectivePoint = ProjectivePoint::random(&mut OsRng);
 
+        // start Google server in a separate thread
         let handle = std::thread::spawn(move || {
             sim_google(&mut ca_clone, &mut g);
         });
@@ -31,20 +34,24 @@ mod tests {
         let username = "alice";
         let pw = "12345";
 
-        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, _k3_s) = pq_tls(&mut stream, &mut ca, ad);
+        // register return true if an error occurred
+        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, _k3_s) = client::pqtls::pq_tls(&mut stream, &mut ca, ad);
         assert!(!client::opaque_register::register(k3_c, &mut stream, &mut aead_nonce, &ad, &username, &pw));
 
-        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = pq_tls(&mut stream, &mut ca, ad);
+        // login return true if an error occurred
+        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = client::pqtls::pq_tls(&mut stream, &mut ca, ad);
         assert!(!client::opaque_login::login(k3_c, k3_s, &mut stream, &mut aead_nonce, &ad, g, &username, &pw));
 
         drop(stream);
 
         handle.join().unwrap();
 
-        println!("Test register_and_login finished.\n\n");
+        println!("Test register_and_login finished.");
+        println!("-------------------------------------------------------\n\n");
     }
 
     fn sim_google(ca: &mut CA, g: &mut ProjectivePoint) {
+        // simulate Google server for register and login test
         let listener = TcpListener::bind("127.0.0.1:9001").unwrap();
         let (mut stream, _) = listener.accept().unwrap();
 
@@ -52,6 +59,7 @@ mod tests {
         let ad = b"Alice,Google,";
         let mut database: HashMap<Vec<u8>, DatabaseContent> = HashMap::new();
 
+        // start pqtls and receive message from Alice
         let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, _k3_s) = server::pqtls::pq_tls(&mut stream, ca, ad);
 
         let msg = User::recv_bytes(&mut stream);
@@ -71,6 +79,7 @@ mod tests {
         let mut username = parts.next().unwrap_or(&[]);
         let mut content = parts.next().unwrap_or(&[]);
 
+        // register return true if an error occurred
         assert!(!server::opaque_register::register(
             &mut aead_nonce,
             &ad,
@@ -80,6 +89,7 @@ mod tests {
             &mut content
         ));
 
+        // start pqtls and receive message from Alice
         let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = server::pqtls::pq_tls(&mut stream, ca, ad);
 
         let msg = User::recv_bytes(&mut stream);
@@ -99,6 +109,7 @@ mod tests {
         let mut username = parts.next().unwrap_or(&[]);
         let mut content = parts.next().unwrap_or(&[]);
 
+        // login return true if an error occurred
         assert!(!server::opaque_login::login(
             k3_c,
             k3_s,
@@ -117,10 +128,13 @@ mod tests {
 
     #[test]
     fn test_wrong_password_and_username() {
+        println!("Testing login with wrong username and password");
+
         let mut ca = CA::new();
         let mut ca_clone = ca.clone();
         let mut g: ProjectivePoint = ProjectivePoint::random(&mut OsRng);
 
+        // start Google server in a separate thread
         let handle = std::thread::spawn(move || {
             sim_google_for_error_case(&mut ca_clone, &mut g);
         });
@@ -136,24 +150,32 @@ mod tests {
         let wrong_username = "alice2";
         let wrong_pw = "123456";
 
-        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, _k3_s) = pq_tls(&mut stream, &mut ca, ad);
+        // register return true if an error occurred
+        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, _k3_s) = client::pqtls::pq_tls(&mut stream, &mut ca, ad);
         assert!(!client::opaque_register::register(k3_c, &mut stream, &mut aead_nonce, &ad, &username, &pw));
 
-        // login return true if an error occurred
-        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = pq_tls(&mut stream, &mut ca, ad);
+        // Test wrong password
+        // login return true if an error occurred. Error is expected here.
+        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = client::pqtls::pq_tls(&mut stream, &mut ca, ad);
         assert!(client::opaque_login::login(k3_c, k3_s, &mut stream, &mut aead_nonce, &ad, g, &username, &wrong_pw));
+        // In case of a wrong password, the client needs to send a reset request
         User::send_bytes(&mut stream, &Message::Reset {});
-        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = pq_tls(&mut stream, &mut ca, ad);
+
+        // Test wrong username
+        // login return true if an error occurred. Error is expected here.
+        let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = client::pqtls::pq_tls(&mut stream, &mut ca, ad);
         assert!(client::opaque_login::login(k3_c, k3_s, &mut stream, &mut aead_nonce, &ad, g, &wrong_username, &pw));
 
         drop(stream);
 
         handle.join().unwrap();
 
-        println!("Test register_and_login finished.\n\n");
+        println!("Test register_and_login finished.");
+        println!("-------------------------------------------------------\n\n");
     }
 
     fn sim_google_for_error_case(ca: &mut CA, g: &mut ProjectivePoint) {
+        // simulate Google server for register and login test with wrong username and password
         let listener = TcpListener::bind("127.0.0.1:9002").unwrap();
         let (mut stream, _) = listener.accept().unwrap();
 
@@ -161,6 +183,7 @@ mod tests {
         let ad = b"Alice,Google,";
         let mut database: HashMap<Vec<u8>, DatabaseContent> = HashMap::new();
 
+        // start pqtls and receive message from Alice
         let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, _k3_s) = server::pqtls::pq_tls(&mut stream, ca, ad);
 
         let msg = User::recv_bytes(&mut stream);
@@ -180,6 +203,7 @@ mod tests {
         let mut username = parts.next().unwrap_or(&[]);
         let mut content = parts.next().unwrap_or(&[]);
 
+        // register return true if an error occurred.
         assert!(!server::opaque_register::register(
             &mut aead_nonce,
             &ad,
@@ -189,6 +213,8 @@ mod tests {
             &mut content
         ));
 
+        // Test wrong password.
+        // start pqtls and receive message from Alice
         let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = server::pqtls::pq_tls(&mut stream, ca, ad);
 
         let msg = User::recv_bytes(&mut stream);
@@ -208,6 +234,7 @@ mod tests {
         let mut username = parts.next().unwrap_or(&[]);
         let mut content = parts.next().unwrap_or(&[]);
 
+        // login return true if an error occurred. Error is expected here.
         assert!(server::opaque_login::login(
             k3_c,
             k3_s,
@@ -220,6 +247,8 @@ mod tests {
             &mut content
         ));
 
+        // Test wrong username.
+        // start pqtls and receive message from Alice
         let (_k1_c, _k1_s, _k2_c, _k2_s, k3_c, k3_s) = server::pqtls::pq_tls(&mut stream, ca, ad);
 
         let msg = User::recv_bytes(&mut stream);
@@ -239,6 +268,7 @@ mod tests {
         let mut username = parts.next().unwrap_or(&[]);
         let mut content = parts.next().unwrap_or(&[]);
 
+        // login return true if an error occurred. Error is expected here.
         assert!(server::opaque_login::login(
             k3_c,
             k3_s,
@@ -250,14 +280,17 @@ mod tests {
             &mut username,
             &mut content
         ));
+        // In case of a wrong username, the server needs to send a reset request
         User::send_bytes(&mut stream, &Message::Reset {});
 
         drop(stream);
         drop(listener);
     }
-    
+
     #[test]
     fn test_double_ratchet() {
+        println!("Testing double_ratchet");
+
         let ad = b"Alice,Google,";
         let mut k3_c = [0u8; 32];
         OsRng.fill_bytes(&mut k3_c);
@@ -274,7 +307,8 @@ mod tests {
         let mut y_i = Scalar::random(&mut OsRng);
         let message_1_from_user = "Hello, world!";
         let message_2_from_user = "How are you?";
-        
+
+        // start Google server in a separate thread
         let handle = std::thread::spawn(move || {
             sim_google_ratchet(&mut g, &mut sk, &mut x_i, &mut y_i, &k3_c.clone(), &k3_s.clone(), message_1_from_user, message_2_from_user);
         });
@@ -287,35 +321,42 @@ mod tests {
         let aead_nonce = &mut [0u8; 12];
         OsRng.fill_bytes(aead_nonce);
 
-
+        // start first double_ratchet iteration with first message
         let (x_i_plus_1, large_y_plus_one, rk_i_plus_2, output) =
             match client::double_ratchet::double_ratchet_iteration(&mut&mut stream, aead_nonce, &&ad, g, &k3_c, &k3_s, rk_i, large_y_i, message_1_from_user) {
             Ok(value) => value,
             Err(value) => panic!("Alice: Error in inner_double_ratchet: {value}"),
         };
-
+        // check if output is correct
         assert_eq!(output, format!("Echo => {}", message_1_from_user));
 
         rk_i = rk_i_plus_2.into();
         large_y_i = large_y_plus_one;
         x_i = x_i_plus_1;
 
-        let (_, _, _, output_2) =
+        // start second double_ratchet iteration with second message
+        let (x_i_plus_2, large_y_plus_two, rk_i_plus_4, output_2) =
         match client::double_ratchet::double_ratchet_iteration(&mut&mut stream, aead_nonce, &&ad, g, &k3_c, &k3_s, rk_i, large_y_i, message_2_from_user) {
             Ok(value) => value,
             Err(value) => panic!("Alice: Error in inner_double_ratchet: {value}"),
         };
 
+        // check if output is correct and if the ratchet keys and DH values have changed
         assert_eq!(output_2, format!("Echo => {}", message_2_from_user));
+        assert_ne!(rk_i, rk_i_plus_4.into());
+        assert_ne!(large_y_i, large_y_plus_two);
+        assert_ne!(x_i, x_i_plus_2);
 
         drop(stream);
 
         handle.join().unwrap();
 
-        println!("Test double_ratchet finished.\n\n");
+        println!("Test double_ratchet finished.");
+        println!("-------------------------------------------------------\n\n");
     }
 
     fn sim_google_ratchet(g: &mut ProjectivePoint, sk: &mut Output<Sha256>, x_i: &mut Scalar, y_i: &mut Scalar, k3_c: &[u8; 32], k3_s: &[u8; 32], message_1_from_user: &str, message_2_from_user: &str) {
+        // simulate Google server for double_ratchet test
         let listener = TcpListener::bind("127.0.0.1:9003").unwrap();
         let (mut stream, _) = listener.accept().unwrap();
         let aead_nonce = &mut [0u8; 12];
@@ -324,24 +365,28 @@ mod tests {
 
         let mut rk_i = sk;
         let mut _large_x_i = g.clone() * x_i.clone();
-        let mut y_i = y_i;
+        let y_i = y_i;
 
+        // start first double_ratchet iteration with first message
         let (large_x_plus_one, y_i_plus_1, mut rk_i_plus_2, output) = match server::double_ratchet::double_ratchet_iteration(&k3_c, &k3_s, &mut &mut stream, aead_nonce, &&ad, *g, *rk_i, *y_i) {
             Ok(value) => value,
             Err(value) => panic!("Google: Error in inner_double_ratchet: {value}"),
         };
 
+        // check if output is correct
         assert_eq!(output, message_1_from_user);
 
         rk_i = (&mut rk_i_plus_2).into();
         _large_x_i = large_x_plus_one;
         *y_i = y_i_plus_1;
 
+        // start second double_ratchet iteration with second message
         let (_, _, _, output_2) = match server::double_ratchet::double_ratchet_iteration(&k3_c, &k3_s, &mut &mut stream, aead_nonce, &&ad, *g, *rk_i, *y_i) {
             Ok(value) => value,
             Err(value) => panic!("Google: Error in inner_double_ratchet: {value}"),
         };
 
+        // check if output is correct
         assert_eq!(output_2, message_2_from_user);
 
         drop(stream);
@@ -350,13 +395,17 @@ mod tests {
 
     #[test]
     fn test_pqtls() {
+        println!("Testing pqtls");
+
         let ad = b"Alice,Google,";
         let mut ca = CA::new();
         let mut ca_clone = ca.clone();
 
+        // start Google server in a separate thread
         let handle = std::thread::spawn(move || {
             let listener = TcpListener::bind("127.0.0.1:9004").unwrap();
             let (mut stream, _) = listener.accept().unwrap();
+            // start pqtls on server side
             let (k1_c, k1_s, k2_c, k2_s, k3_c, k3_s) = server::pqtls::pq_tls(&mut stream, &mut ca_clone, ad);
 
             drop(stream);
@@ -369,11 +418,13 @@ mod tests {
 
         let mut stream = TcpStream::connect("127.0.0.1:9004").unwrap();
 
+        // start pqtls on client side
         let (alice_k1_c, alice_k1_s, alice_k2_c, alice_k2_s, alice_k3_c, alice_k3_s) = client::pqtls::pq_tls(&mut stream, &mut ca, ad);
 
         let result = handle.join().unwrap();
         let (google_k1_c, google_k1_s, google_k2_c, google_k2_s, google_k3_c, google_k3_s) = result;
 
+        // compare client and server keys
         assert_eq!(alice_k1_c, google_k1_c);
         assert_eq!(alice_k1_s, google_k1_s);
         assert_eq!(alice_k2_c, google_k2_c);
@@ -383,6 +434,7 @@ mod tests {
 
         drop(stream);
 
-        println!("Test pqtls finished.\n\n");
+        println!("Test pqtls finished.");
+        println!("-------------------------------------------------------\n\n");
     }
 }
